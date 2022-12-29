866de3019d5904a462589b0f266b0acaf813fe1e
Ramlen97 <rameshwarlenka97@gmail.com>
2022-12-29 23:49:09 +0530

    ServingHtmlCss

--- Node First App/app.js
+++ Node First App/app.js
@@ -1,5 +1,7 @@
 // app.js
+const path=require('path');
 const express=require('express');
+
 const bodyParser=require('body-parser');
 
 const app= express();
@@ -8,12 +10,13 @@ const adminRoutes= require('./routes/admin.js');
 const shopRoutes= require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use('/shop',shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 });
 
 app.listen(3000);
