eec8deec10fb3eb8e773696c4506924ecef83f43
Ramlen97 <rameshwarlenka97@gmail.com>
2023-01-01 20:24:49 +0530

    modelsAndDynamicRoutes

--- Node First App/app.js
+++ Node First App/app.js
@@ -1,27 +1,24 @@
-// app.js
-const path=require('path');
-const express=require('express');
+const path = require('path');
 
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app= express();
+const errorController = require('./controllers/error');
 
-const adminRoutes= require('./routes/admin.js');
-const shopRoutes= require('./routes/shop.js');
-const contactusRoutes=require('./routes/contactus');
-const successRoutes=require('./routes/success');
+const app = express();
 
-const errorController=require('./controllers/error');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname,'public')));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
-app.use('/contactusform',contactusRoutes);
-app.use('/success',successRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.error404);
+app.use(errorController.get404);
 
 app.listen(3000);
